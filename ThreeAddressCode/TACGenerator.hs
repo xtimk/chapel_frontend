c88aa64badc7ba3f5751927efa9d6e8e779aaee5
darko <dellapietradarko@yahoo.it>
2020-11-01 17:39:19 +0100

    Start TAC

--- ThreeAddressCode/TACGenerator.hs
+++ ThreeAddressCode/TACGenerator.hs
@@ -10,10 +10,49 @@ startTacState bpTree = ([], 0, bpTree)
 tacGenerator (Progr p) = tacGeneratorModule p
 
 tacGeneratorModule (Mod m) = do
-  typeCheckerExt m
+  tacGeneratorExt m
   get
 
-tacGeneratorExt [] = get
-typeCheckerExt (x:xs) = case x of
-    ExtDecl (Decl decMode declList _ ) -> get
-    ExtFun fun -> get
+tacGeneratorExt (x:xs) = case x of
+    ExtDecl (Decl decMode declList _ ) -> do
+        mapM_ tacGeneratorDeclaration declList
+        tacGeneratorExt xs
+    ExtFun fun -> -- do
+        --tacGeneratorFunction fun
+        tacGeneratorExt xs
+
+tacGeneratorDeclaration x = do
+  environment <- get
+  case x of
+    NoAssgmDec ids _colon types -> get
+    AssgmDec ids assignment exp ->
+      let temp = tacGeneratorDeclExpression environment exp in do
+            tacGeneratorIdentifiers temp ids
+            get
+    AssgmTypeDec ids _colon types assignment exp -> get
+      --let DataChecker ty errors = typeCheckerDeclExpression environment exp in do
+      --      typeCheckerIdentifiers ids (Just assignment) (convertTypeSpecToTypeInferred types) ty
+      --      modify $ addErrorsCurrentNode errors
+      --      get
+    NoAssgmArrayFixDec ids _colon array -> get --typeCheckerIdentifiersArray ids Nothing array Infered Infered
+    NoAssgmArrayDec ids _colon array types -> get --typeCheckerIdentifiersArray ids  Nothing array (convertTypeSpecToTypeInferred types) Infered
+    AssgmArrayTypeDec ids _colon array types assignment exp ->  get
+      --let DataChecker ty errors = typeCheckerDeclExpression environment exp in do
+       --     typeCheckerIdentifiersArray ids (Just assignment) array (convertTypeSpecToTypeInferred types) ty
+       --    modify $ addErrorsCurrentNode errors
+        --    get
+    AssgmArrayDec ids _colon array assignment exp ->  get
+     -- let DataChecker ty errors = typeCheckerDeclExpression environment exp in do
+       --     typeCheckerIdentifiersArray ids (Just assignment) array Infered ty
+         --   modify $ addErrorsCurrentNode errors
+         --   get
+
+tacGeneratorIdentifiers = map tacGeneratorIdentifier
+tacGeneratorIdentifier temp id = do
+    modify $ addTacEntry $ TACEntry Nothing (Nullary (Temp temp (-1,-1)))
+    get
+
+addTacEntry tacEntry env@(tac, _t, _b ) =
+    (tacEntry:tac, _t,_b)
+
+
