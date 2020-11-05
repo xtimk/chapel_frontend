3921bf2573982d1e8afbe48a87062da375b4bb06
darko <dellapietradarko@yahoo.it>
2020-11-05 23:24:47 +0100

    small modification

--- Checker/BPTree.hs
+++ Checker/BPTree.hs
@@ -8,8 +8,6 @@ import Prelude hiding (id)
 import qualified Data.Map as DMap
 import Checker.ErrorPrettyPrinter
 
-import Data.List
-
 data BPTree a = Void | Node {
     id :: (String, (Loc, Loc)),
     val :: a,
@@ -53,7 +51,7 @@ findProcedureGetBlkType tree current_id =
             ExternalBlk -> ExternalBlk
             _otherwhise -> findProcedureGetBlkType tree (getParentID node)
 
-getBlkTypeSimple tree@(Node _ (BP _ _ _ blocktype) _ _) = blocktype
+getBlkTypeSimple (Node _ (BP _ _ _ blocktype) _ _) = blocktype
 
 findSequenceControlGetBlkType tree current_id = 
     let node = findNodeById current_id tree in
@@ -66,7 +64,7 @@ findSequenceControlGetBlkType tree current_id =
             ExternalBlk -> ExternalBlk -- sono arrivato ricorsivamente fino al blocco esterno, lo restituisco, non vado in ricorsione
             _otherwhise -> findSequenceControlGetBlkType tree (getParentID node)
 
-getParentID tree@(Node _ _ parent _) = 
+getParentID (Node _ _ parent _) = 
     case parent of
         Just p -> p
         -- il caso nothing non serve, non e' mai raggiungibile dalla getBlkType
@@ -75,35 +73,35 @@ modErrorsPos pos = map (modErrorPos pos)
 
 modErrorPos loc (ErrorChecker _oldloc a) = ErrorChecker loc a
 
-modFunRetType identifier tye tree@(Node id (BP symboltable statements errors blocktype) parent children) = 
+modFunRetType identifier tye (Node id (BP symboltable statements errors blocktype) parent children) = 
     Checker.BPTree.Node {Checker.BPTree.id = id,
     val = BP {symboltable = DMap.adjust (alterRetType tye) identifier symboltable, statements = statements, errors = errors, blocktype = blocktype}, 
     parentID = parent, 
     children = children}
 
-modFunAddOverloading identifier vars tree@(Node id (BP symboltable statements errors blocktype) parent children) = 
+modFunAddOverloading identifier vars (Node id (BP symboltable statements errors blocktype) parent children) = 
     Checker.BPTree.Node {Checker.BPTree.id = id,
     val = BP {symboltable = DMap.adjust (addFunVariables vars) identifier symboltable, statements = statements, errors = errors, blocktype = blocktype}, 
     parentID = parent, 
     children = children}
 
-alterRetType tye (s,(Function _a _b t1 )) = (s,(Function _a _b tye))
+alterRetType tye (s,(Function _a _b _ )) = (s,(Function _a _b tye))
 addFunVariables vars (s,(Function _a varOfVar _t )) = (s,(Function _a (vars:varOfVar) _t))
 
-getSymbolTable tree@(Node _ (BP symboltable _ _ blocktype) _ _) =  symboltable
-setSymbolTable sym tree@(Node id (BP _ statements errors blocktype) parent children) = 
+getSymbolTable (Node _ (BP symboltable _ _ _) _ _) =  symboltable
+setSymbolTable sym (Node id (BP _ statements errors blocktype) parent children) = 
     Checker.BPTree.Node {Checker.BPTree.id = id,
     val = BP {symboltable = sym, statements = statements, errors = errors, blocktype = blocktype}, 
     parentID = parent, 
     children = children}
 
-getErrors tree@(Node _ (BP _ _ errors _) _ _) = errors
+getErrors (Node _ (BP _ _ errors _) _ _) = errors
               
 
 addFunctionOnCurrentNode identifier loc variables types (_s,tree,currentId) = 
     let entry = Function loc variables types in
     (_s,updateTree (addEntryNode identifier entry (findNodeById currentId tree)) tree, currentId)
-addEntryNode identifier entry tree@(Node id (BP symboltable _st _e _bt) _p _c) = 
+addEntryNode identifier entry (Node id (BP symboltable _st _e _bt) _p _c) = 
     Checker.BPTree.Node {Checker.BPTree.id = id,
     val = BP {symboltable = DMap.insert identifier (identifier, entry) symboltable, statements = _st, errors = _e, blocktype = _bt}, 
     parentID = _p, 
@@ -114,23 +112,23 @@ addErrorsCurrentNode errors (_s,tree,currentId) = (_s,updateTree (addErrorsNode 
 
 addErrorsNode :: (Foldable t0) => BPTree BP -> t0 ErrorChecker -> BPTree BP
 addErrorsNode = foldr addErrorNode
-addErrorNode errorChecker tree@(Node id (BP symboltable statements errors blocktype) parent children) = 
+addErrorNode errorChecker (Node id (BP symboltable statements errors blocktype) parent children) = 
     Checker.BPTree.Node {Checker.BPTree.id = id,
     val = BP {symboltable = symboltable, statements = statements, errors = errorChecker:errors, blocktype = blocktype}, 
     parentID = parent, 
     children = children}
 
 updateTree _ Checker.BPTree.Void = Checker.BPTree.Void
-updateTree actualNode@(Node idActual val parentId childrenActualNode) entireTree@(Node idEntire  _a _b children) = if idEntire == idActual
+updateTree actualNode@(Node idActual _ _ _) (Node idEntire  _a _b children) = if idEntire == idActual
     then actualNode
     else Node idEntire _a _b (map (updateTree actualNode) children)
 
-addChild actualNode@(Node idActual val parentId childrenActualNode) childNodeToAdd Checker.BPTree.Void = Checker.BPTree.Void
-addChild actualNode@(Node idActual val parentId childrenActualNode) childNodeToAdd entireTree@(Node idEntire _a _b children)  = if idEntire == idActual
+addChild (Node _ _ _ _) _ Checker.BPTree.Void = Checker.BPTree.Void
+addChild actualNode@(Node idActual val parentId childrenActualNode) childNodeToAdd (Node idEntire _a _b children)  = if idEntire == idActual
     then Node idActual val parentId (reverse (childNodeToAdd:childrenActualNode))
     else Node idEntire _a _b (map (addChild actualNode childNodeToAdd) children)
 
-createChild identifier blkType locStart locEnd tree@(Node id val parent children) = Checker.BPTree.Node 
+createChild identifier blkType locStart locEnd (Node id _ _ _) = Checker.BPTree.Node 
     {Checker.BPTree.id = (identifier,(locStart,locEnd)), 
     parentID = Just id, 
     val = BP {symboltable = DMap.empty, statements = [], errors = [], blocktype = blkType}, 
@@ -161,21 +159,21 @@ getVarTypeTAC (PIdent (loc, identifier)) (_,_,_,tree)  =
             Just (_,Function loc _ t) -> (loc,t)
             -- Nothing -> DataChecker Checker.SymbolTable.Error [ErrorChecker (l,c) $ Checker.SymbolTable.ErrorVarNotDeclared identifier]
 
-getFunParams (PIdent ((l,c), identifier)) (_,tree,currentNode) = 
+getFunParams (PIdent (_, identifier)) (_,tree,currentNode) = 
     let symtable = uniteSymTables $ bpPathToList currentNode tree in
         case DMap.lookup identifier symtable of
             Just (_,Function _ params _) -> params
 
 getFunRetType env@(s,tree,current_id) = 
-  let n@(Node (id, _) (BP _ _ _ blkTy) pId _) = findNodeById current_id tree in
+  let (Node (id, _) (BP _ _ _ blkTy) pId _) = findNodeById current_id tree in
     case pId of
       Just parID ->
         case blkTy of
           ProcedureBlk -> getVarType (PIdent ((0,0), id)) env
           _otherwhise -> getFunRetType (s,tree,parID)
 
-getFunNameFromEnv env@(_s,tree,current_id) = 
-  let n@(Node (id,_) (BP _ _ _ blkTy) (Just parID) _) = findNodeById current_id tree in
+getFunNameFromEnv (_s,tree,current_id) = 
+  let (Node (id,_) (BP _ _ _ blkTy) (Just parID) _) = findNodeById current_id tree in
     case blkTy of
       ProcedureBlk -> id
       _otherwhise -> getFunNameFromEnv (_s,tree,parID)
@@ -184,13 +182,13 @@ getFunNameFromEnv env@(_s,tree,current_id) =
 setReturnType ty env@(_,_,actualId) = setReturnType' actualId ty env
     where
         setReturnType' actualId ty (_s,tree,current_id) =
-            let n@(Node (id, _) (BP _ _ _ blkTy) (Just parID) _) = findNodeById current_id tree in
+            let (Node (id, _) (BP _ _ _ blkTy) (Just parID) _) = findNodeById current_id tree in
                 case blkTy of
                 ProcedureBlk -> let node = findNodeById parID tree in (_s, updateTree (modFunRetType id ty node) tree , actualId )
                 _otherwhise -> setReturnType' actualId ty (_s,tree,parID)
 
 
-uniteSymTables = foldr (\y@(Node _ (BP sym _ _errors _) _ _) x -> DMap.union sym x ) DMap.empty
+uniteSymTables = foldr (\(Node _ (BP sym _ _errors _) _ _) x -> DMap.union sym x ) DMap.empty
 
 bpPathToList = bpPathToList' []     
     where
@@ -213,19 +211,13 @@ bp2list = bpttolist []
         bpttolist xs x@(Node _ _ _ children) =  x : concatMap (bpttolist xs) children
 
 
-getCurIdOfTokenPos loc tree@(Node id@(_,((lstart,cstart),(lend,cend))) _ _ children) = 
+getCurIdOfTokenPos loc (Node id _ _ children) = 
   let r = filter (isIdInTree loc) children in
     if null r
       then id
       else getCurIdOfTokenPos loc (head r)
 
--- getSymTable loc tree = 
---   let currentNode = getCurIdOfTokenPos loc tree in
---     uniteSymTables $ bpPathToList currentNode tree
--- getVarType (PIdent (loc,identifier)) (_,tree,currentNode)
-
-
-isIdInTree loc@(l,c)  (Node (_,((lstart,cstart),(lend,cend))) _ _ _) --  start@(lstart, cstart) end@(lend, cend)
+isIdInTree (l,c)  (Node (_,((lstart,cstart),(lend,cend))) _ _ _) 
   | l < lstart = False
   -- a
   --{
