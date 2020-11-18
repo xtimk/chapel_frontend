b14148dcc9b1978cd2a48c299acaebc41a8a832a
xtimk <x-timk>
2020-11-18 22:14:02 +0100

    feat: adding assignment type in tac print

--- ThreeAddressCode/TACPrettyPrinter.hs
+++ ThreeAddressCode/TACPrettyPrinter.hs
@@ -8,13 +8,24 @@ printTacEntries = mapM_ printTacEntry
 
 printTacEntry (TACEntry label operation) = putStrLn $ printLabel label ++ printTacEntry' operation
 
+printTacEq tye = " =_" ++ printTacEqAux tye ++ " "
+
+printTacEqAux tye = 
+    case tye of
+        (Pointer p) -> "addr"
+        _ -> map toLower $ show tye
+
 printTacEntry' operation = case operation of
     Binary temp1 temp2 bop temp3 -> -- printTacTemp temp1 ++ " = " ++ printTacTemp temp2 ++  printTacBop bop tye1 ++ printTacTemp temp3
-        let tye1 = getTacTempTye temp2
+        let tye0 = getTacTempTye temp1
+            tye1 = getTacTempTye temp2
             tye2 = getTacTempTye temp3 in
-        printTacTemp temp1 ++ " = " ++ printTacTemp temp2 ++  printTacBop bop tye1 ++ printTacTemp temp3
-    Unary temp1 uop  temp2 -> printTacTemp temp1 ++ " = " ++ printTacUop uop ++ printTacTemp temp2
-    Nullary temp1 temp2 ->  printTacTemp temp1 ++ " = " ++ printTacTemp temp2
+        printTacTemp temp1 ++ printTacEq tye0 ++ printTacTemp temp2 ++  printTacBop bop tye1 ++ printTacTemp temp3
+    Unary temp1 uop  temp2 -> 
+        let tye0 = getTacTempTye temp1 in
+        printTacTemp temp1 ++ printTacEq tye0 ++ printTacUop uop ++ printTacTemp temp2
+    Nullary temp1 temp2 -> let tye0 = getTacTempTye temp1 in
+        printTacTemp temp1 ++ printTacEq tye0 ++ printTacTemp temp2
     UnconJump label -> "goto " ++ printLabelGoto label
     BoolTrueCondJump temp label -> "if " ++ printTacTemp temp ++ " goto " ++ printLabelGoto label
     BoolFalseCondJump temp label -> "ifFalse " ++ printTacTemp temp ++ " goto " ++ printLabelGoto label
@@ -23,19 +34,19 @@ printTacEntry' operation = case operation of
             tye2 = getTacTempTye temp2 in
         "if " ++ printTacTemp temp1 ++ printTacRel rel tye1 ++ printTacTemp temp2 ++ " goto " ++ printLabelGoto label
     IndexLeft temp1 temp2 temp3 ->  printTacTemp temp1 ++ "[" ++ printTacTemp temp2 ++ "]" ++ " = " ++ printTacTemp temp3
-    IndexRight temp1 temp2 temp3 -> printTacTemp temp1 ++ " = " ++  printTacTemp temp2 ++  "[" ++ printTacTemp temp3 ++ "]"
-    DeferenceRight temp1 temp2 -> printTacTemp temp1 ++ " = get_address " ++  printTacTemp temp2
-    ReferenceLeft temp1 temp2 -> "*" ++ printTacTemp temp1 ++ " = " ++  printTacTemp temp2
-    ReferenceRight temp1 temp2 -> printTacTemp temp1 ++ " = *" ++  printTacTemp temp2
+    IndexRight temp1 temp2 temp3 -> printTacTemp temp1 ++ printTacEq (getTacTempTye temp1) ++  printTacTemp temp2 ++  "[" ++ printTacTemp temp3 ++ "]"
+    DeferenceRight temp1 temp2 -> printTacTemp temp1 ++ printTacEq (getTacTempTye temp1) ++ "get_address " ++  printTacTemp temp2
+    ReferenceLeft temp1 temp2 -> "*" ++ printTacTemp temp1 ++ printTacEq (getTacTempTye temp1) ++  printTacTemp temp2
+    ReferenceRight temp1 temp2 -> printTacTemp temp1 ++ printTacEq (getTacTempTye temp1) ++ "*" ++  printTacTemp temp2
     SetParam temp -> "param " ++ printTacTemp temp 
     CallProc temp1 arity -> "call " ++ printTacTemp temp1 ++ ", " ++ show arity 
     CallFun temp1 temp2 arity -> printTacTemp temp1 ++ " = fcall " ++ printTacTemp temp2 ++ ", " ++ show arity 
     ReturnVoid -> "return"
     ReturnValue temp -> "return " ++ printTacTemp temp
     VoidOp -> ""
-    Cast temp1 CastIntToFloat temp2 -> printTacTemp temp1 ++ " = " ++ "cast_int_to_float " ++ printTacTemp temp2
-    Cast temp1 CastCharToInt temp2 -> printTacTemp temp1 ++ " = " ++ "cast_char_to_int " ++ printTacTemp temp2
-    Cast temp1 CastCharToReal temp2 -> printTacTemp temp1 ++ " = " ++ "cast_char_to_real " ++ printTacTemp temp2
+    Cast temp1 CastIntToFloat temp2 -> printTacTemp temp1 ++ printTacEq (getTacTempTye temp1) ++ "cast_int_to_float " ++ printTacTemp temp2
+    Cast temp1 CastCharToInt temp2 -> printTacTemp temp1 ++ printTacEq (getTacTempTye temp1) ++ "cast_char_to_int " ++ printTacTemp temp2
+    Cast temp1 CastCharToReal temp2 -> printTacTemp temp1 ++ printTacEq (getTacTempTye temp1) ++ "cast_char_to_real " ++ printTacTemp temp2
 
 -- tacsup Int Int = Int
 -- tacsup Real Real = Real
